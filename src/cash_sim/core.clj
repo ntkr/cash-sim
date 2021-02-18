e0265257bd2a243bde76199d831161cdf0cc7c5a
lux <tucker.ian@gmail.com>
2021-02-17 21:35:15 -0600

    abstracted repeating events, merging events. Also added ability to apply changes described in an event to a set of accounts.

--- src/cash_sim/core.clj
+++ src/cash_sim/core.clj
@@ -10,22 +10,64 @@
   (println "Hello, World!"))
 
 
-;; list of transfers
-;; (:type 'transfer :amount 100 :from 'tacobell :to 'ally :everydays "14" :beginning "1/1/2020")
+(defn precedes? [d1 d2] (> 0 (compare d1 d2)))
+(defn sameday? [d1 d2] (= 0 (compare d1 d2)))
 
-(defn calc-transfers [& {:keys [amount from to everydays beginning]}]
-  (for [date (iterate plus beginning (days everydays))] 
-    {:type   'transfer 
-     :from   from 
-     :to     to 
-     :amount amount 
-     :date   date}))
+;; create instances of x with dates ranging from begin to end with interval-days days 
+(defn repeat-event [x begin end intervalct intervaltype]
+  (for [date (iterate plus begin (intervaltype intervalct))
+       :while (precedes? date end)]
+    (merge x {:date date})))
 
-(defn construct-event-list [beginning end event-generator]
-  (-> event-generator
-    (drop-while #(< beginning %1)) ;; use compare here
-    (take-while #(< end %1))))
+(defn merge-events [& events]
+  (sort-by :date (flatten events)))
 
-;; (defn simulate [accounts beginning end event-generators]
-;;   (let [event])
-;;   ())
+
+(defmulti process-event (fn [event accts] (:type event)))
+(defmethod process-event :transfer [event accts]
+  (-> accts
+    (update (:from event) #(- % (:amount event)))
+    (update (:to event) #(+ % (:amount event)))))
+
+(defn reduce-event [accts event]
+  (let [next-accts (process-event event (last accts))]
+    (conj (vec accts) next-accts)))
+
+;;;; PLAYGROUND ;;;;
+
+(def accts {
+  :tacobell 1000000 
+  :ally 320
+  :boa 100
+})
+
+(def paychecks
+  (repeat-event
+    {
+      :type :transfer
+      :amount 1000
+      :from :tacobell 
+      :to :ally 
+    }
+    (local-date "2020-01-01")
+    (local-date "2021-01-01")
+    14
+    days
+  ))
+
+(def stipend
+  (repeat-event
+    {
+      :type :transfer
+      :amount 800
+      :from :ally
+      :to :boa
+    }
+    (local-date "2020-01-01")
+    (local-date "2021-01-01")
+    1
+    months
+  ))
+
+;; (reduce process-event accts events)
+;; (def ev (first paychecks))
